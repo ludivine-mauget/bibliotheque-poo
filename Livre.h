7be35ec62ae827bcde2bbc06ca6d9b8b7c191b88
dmassif <damien.massif@gmail.com>
2024-01-08 19:35:46 +0100

    update class Livre

---
+++ Livre.h
@@ -0,0 +1,45 @@
+//
+// Created by Massif on 08/01/2024.
+//
+
+#ifndef BIBLIO_LIVRE_H
+#define BIBLIO_LIVRE_H
+
+#include <iostream>
+
+using namespace std;
+
+
+class Livre {
+public:
+    Livre(int code, const string &auteur, const string &titre, const string &editeur, const string &isbn,
+          const string &public_vise);
+    Livre();
+    int getCode() const;
+    void setCode(int code);
+    const string &getAuteur() const;
+    void setAuteur(const string &auteur);
+    const string &getTitre() const;
+    void setTitre(const string &titre);
+    const string &getEditeur() const;
+    void setEditeur(const string &editeur);
+    const string &getIsbn() const;
+    void setIsbn(const string &isbn);
+    const string &getPublicVise() const;
+    void setPublicVise(const string &publicVise);
+    virtual void affiche();
+    enum types {LIVRE, BD, POESIE, ROMAN, PIECE, ALBUM};
+
+private:
+    int type;
+    int code{};
+    string auteur;
+    string titre;
+    string editeur;
+    string ISBN;
+    string publicVise;
+    emprunteur* emprunte_par{}; // a changer
+};
+
+
+#endif //BIBLIO_LIVRE_H
