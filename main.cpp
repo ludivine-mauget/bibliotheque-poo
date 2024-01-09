a649290414a0dfd3d6c79c5530e3f5aaf6dd6b46
Lustucru <ludivine.mauget@centrale-marseille.fr>
2024-01-09 10:28:08 +0100

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Ludivine Mauget on 09/01/2024.
+//
+
+#include "BD.h"
+#include "Album.h"
+#include "Bibliotheque.h"
+#include "Adherent.h"
+#include "Livre.h"
+#include "Poesie.h"
+#include "Roman.h"
+#include "Theatre.h"
+
+#include <iostream>
+
+using namespace std;
+
