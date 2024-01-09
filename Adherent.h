c9434dc7d91dbf033697a570c4bb1f68d081f5ee
acaradec <70711295+acaradec@users.noreply.github.com>
2024-01-09 11:55:16 +0100

    Update Adherent.h

--- Adherent.h
+++ Adherent.h
@@ -1,23 +1,36 @@
 #include <iostream>
-using namesapce std;
+using namespace std;
 #include "Bibliotheque.h"
 #include "Livre.h"
 
-class Adhérent
+class Adherent
 {
-private :
-  string nom;
-  string prénom;
-  string adresse;
-  static int nbAdhérent;
-  int numAdhérent;
-  Blibliothèque bibliothèque;
-  Livre* livreEmpruntés;
-  int maxEmprunt;
-public :
-  Adhérent(); //assigne un num d'adhérent
-  Adhérent(String n, String p); //avec nom, prémnom
-  ~Adhérent();
-  void Emprunter(String code);
-  void Rendre(Livre livre);
+private:
+	string nom;
+	string prenom;
+	string adresse;
+	static int IdAdherent;
+	int nbAdherent;
+	Bibliotheque bibliotheque;
+	Livre* livreEmpruntes;
+	int maxEmprunt;
+public:
+	Adherent(); //assigne un num d'adherent
+	Adherent(string n, string p); //avec nom, premnom
+	~Adherent();
+	void Emprunter();
+	void Rendre();
+
+	const string& getNom() const;
+	void setNom(const string& nom);
+	const string& getPrenom() const;
+	void setPrenom(const string& prenom);
+	const string& getAdresse() const;
+	void setAdresse(const string& adesse);
+	const int& getNbAdherent() const;
+	const Bibliotheque& getBibliotheque() const;
+	void setBibliotheque(const Bibliotheque& biblio);
+	const Livre* getLivres() const;
+	const int& getMaxEmprunt() const;
+	void setMaxEmprunt(const int& seuil);
 }
