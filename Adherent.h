43559e31dead4385cdaaa117ad12826b9650bf27
acaradec <70711295+acaradec@users.noreply.github.com>
2024-01-14 22:26:33 +0100

    Update Adherent.h

--- Adherent.h
+++ Adherent.h
@@ -24,10 +24,12 @@ private:
     int idAdherent;
     Bibliotheque* bibliotheque;
     vector<Livre*> livresEmpruntes; //liste chaînée
+    int nbEmprunt;
+    int nbMaxEmprunt;
 public:
     static int nbAdherent;
 
-    Adherent(string nom, string prenom, string adresse, Bibliotheque* bibliotheque);
+    Adherent(string nom, string prenom, string adresse, Bibliotheque* bibliothequen, int maxEmprunt);
     ~Adherent();
     const string& getNom() const;
     void setNom(const string& nom);
@@ -36,8 +38,10 @@ public:
     const string& getAdresse() const;
     void setAdresse(const string& adresse);
     const int& getIdAdherent() const;
+    void setMaxEmprunt(const int& max);
+    const int& getMaxEmprunt() const;
 
-    void emprunterLivre(string codeLivre);
+    void emprunterLivre(int codeLivre);
     void rendreLivre(Livre* livre);
     void afficheAdherent();
 
