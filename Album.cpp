063dab22ed0e49e6f0f928c9f2b6b3b1f5f0d84b
acaradec <70711295+acaradec@users.noreply.github.com>
2024-01-14 16:41:46 +0100

    Update Album.cpp

--- Album.cpp
+++ Album.cpp
@@ -2,20 +2,21 @@
 
 Album::Album() :Livre()
 {
-	illustration = BOTH;
+	illustration = "";
 }
 
-Album::Album(int code, const string& auteur, const string& titre, const string& editeur, const string& isbn, const string& public_vise, Illu& illu):Livre(code,auteur,titre,editeur,isbn,public_vise)
+Album::Album(const int& code, const string& auteur, const string& titre, const string& editeur, const string& isbn, const string& public_vise, const string& illu):Livre(code,auteur,titre,editeur,isbn,public_vise)
 {
+	this->type = ALBUM;
 	illustration = illu;
 }
 
-const Illu& Album::getIllustration()
+const string& Album::getIllustration()
 {
 	return illustration;
 }
 
-void Album::SetIllustration(const Illu& illustration)
+void Album::SetIllustration(const string& illustration)
 {
 	this->illustration = illustration;
 }
